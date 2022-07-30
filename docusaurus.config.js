451e920681d20800269e53a88f2dc80dc7f28ec9
Andrew <andrew@liseenco.com>
2022-07-30 22:20:53 +1000

    changed to use yarn

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'InfoFlow',
   tagline: 'Information Flow',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  // url: 'https://your-docusaurus-test-site.com',
+  url: 'https://infoflowconnect.github.io/',
+  baseUrl: '/infodocs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'infoflowconnect', // Usually your GitHub org/user name.
+  projectName: 'infodocs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
